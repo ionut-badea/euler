406717b9fe25efdf1153cb2bb1a5f306cb4fb905
jack <ionut.badea@yazee.me>
2019-10-30 20:46:29 +0200

    problem no. 2

--- Euler_001.py
+++ Euler_001.py
@@ -8,7 +8,8 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 def compute():
     result = 0
-    for n in range(1000):
+    maxim = 1000
+    for n in range(maxim):
         if n % 3 == 0 or n % 5 == 0:
             result += n
     return result
